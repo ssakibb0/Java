305bb2c8e0efd417294c19dbbb810c5be4599e10
Mohd Sakib Shaikh <mohdsakib8085@gmail.com>
2025-09-17 15:14:18 +0530

    OOPS concept

---
+++ JavaTrickyQuestionOOPs.java
@@ -0,0 +1,28 @@
+class Parent {
+    // Parent method returning Object
+    public Object show() {
+        System.out.println("Parent show() method");
+        return "Parent";
+    }
+}
+
+class Child extends Parent {
+    // Overriding method with covariant return type (String is a subclass of Object)
+    @Override
+    public String show() {
+        System.out.println("Child show() method");
+        return "Child";
+    }
+}
+
+public class JavaTrickyQuestionOOPs {
+    public static void main(String[] args) {
+        Parent p1 = new Parent();
+        Parent p2 = new Child(); // Polymorphism
+        Child c1 = new Child();
+
+        System.out.println(p1.show()); // Calls Parent's method
+        System.out.println(p2.show()); // Calls Child's method (runtime overriding)
+        System.out.println(c1.show()); // Calls Child's method
+    }
+}
